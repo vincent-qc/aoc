e79bf090279f17d5a4e64d4bf3c209d67a8fc8e0
Vincent Qi <vincent.qi.2006@gmail.com>
2025-12-14 16:27:58 -0800

    day12

--- main.py
+++ main.py
@@ -1,61 +1,111 @@
-from collections import defaultdict
-
 from util import next_line
 
-N = 643
+N = 6
+M = 1000
+
+
+def place(grid, cells, x, y):
+    for (dx, dy) in cells:
+        if grid[y+dy][x+dx] == '#':
+            return False
+    for (dx, dy) in cells:
+        grid[y+dy][x+dx] = '#'
+    return True
+
+
+def remove(grid, cells, x, y):
+    for (dx, dy) in cells:
+        grid[y+dy][x+dx] = '.'
+
+
+def bt(shapes, grid, queue, w, h):
+    if not queue:
+        return True
+
+    cur = queue.pop()
+    for (cells, sw, sh) in shapes[cur]:
+        for y in range(h - sh + 1):
+            for x in range(w - sw + 1):
+                if place(grid, cells, x, y):
+                    if bt(shapes, grid, queue, w, h):
+                        return True
+                    remove(grid, cells, x, y)
+    queue.append(cur)
+    return False
+
+
+def start(shapes, grid):
+    ((w, h), reqs) = grid
+
+    queue = []
+    area = 0
+    for idx in range(len(reqs)):
+        cnt = reqs[idx]
+        queue.extend([idx] * cnt)
+        if shapes[idx]:
+            area += len(shapes[idx][0][0]) * cnt
+
+    if area > w * h:
+        return False
+
+    queue.sort(key=lambda i: -len(shapes[i][0][0]))
+    empty = [["."] * w for _ in range(h)]
+    return bt(shapes, empty, queue, w, h)
+
+
+def norm(cells):
+    mx = min(c[0] for c in cells)
+    my = min(c[1] for c in cells)
+    return tuple(sorted((c[0]-mx, c[1]-my) for c in cells))
+
+
+def permute(shape):
+    def rot(s):
+        return {(2-y, x) for (x, y) in s}
+
+    def flip(s):
+        return {(2-x, y) for (x, y) in s}
+
+    cells = {(x, y) for y in range(3) for x in range(3) if shape[y][x] == '#'}
+
+    seen = set()
+    result = []
+    cur = cells
+    for _ in range(4):
+        for s in [cur, flip(cur)]:
+            n = norm(s)
+            if n not in seen:
+                seen.add(n)
+                sw = max(c[0] for c in n) + 1
+                sh = max(c[1] for c in n) + 1
+                result.append((n, sw, sh))
+        cur = rot(cur)
+
+    return result
 
 
 def solve():
-    edges = defaultdict(list)
-
+    shapes = []
     for _ in range(N):
-        [src, dst] = next_line().split(': ')
-        dsts = dst.split(' ')
-        edges[src] = dsts
+        _ = next_line()
+        l1 = list(next_line())
+        l2 = list(next_line())
+        l3 = list(next_line())
+        shapes.append(permute([l1, l2, l3]))
+        _ = next_line()
 
-    memo = [[{}, {}], [{}, {}]]
+    grids = []
+    for _ in range(M):
+        [dim, req] = next_line().split(": ")
+        [x, y] = map(int, dim.split("x"))
+        grids.append(((x, y), list(map(int, req.split(' ')))))
 
-    def dpget(node, dac, fft):
-        curmemo = memo[dac][fft]
-        if node in curmemo:
-            val = curmemo[node]
-            return (True, val)
-        else:
-            return (False, None)
+    res = 0
+    for grid in grids:
+        if start(shapes, grid):
+            res += 1
 
-    def dpset(node, dac, fft, val):
-        curmemo = memo[dac][fft]
-        curmemo[node] = val
-
-    def dfs(node, path):
-        if node == "out":
-            if "dac" in path and "fft" in path:
-                return 1
-            else:
-                return 0
-
-        if node in path:
-            return 0
-
-        dac = 1 if "dac" in path else 0
-        fft = 1 if "fft" in path else 0
-        (worked, val) = dpget(node, dac, fft)
-        if worked:
-            return val
-
-        path.append(node)
-
-        res = 0
-        for nbor in edges[node]:
-            res += dfs(nbor, path)
-
-        dpset(node, dac, fft, res)
-
-        path.pop()
-
-        return res
-
-    print(dfs("svr", []))
+    print(res)
 
 
 solve()
